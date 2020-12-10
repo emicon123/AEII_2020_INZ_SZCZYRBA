bb37bd2fd3e981351327d465009e1c17f69f95d6
Wojciech <wojciech.szczyrba@clarite.pl>
2020-12-10 17:58:44 +0100

    Added volume and color props to preview screen.

--- modules/Heatmap/src/main/java/org/gephi/plugins/preview/heatmap/HeatmapRenderer.java
+++ modules/Heatmap/src/main/java/org/gephi/plugins/preview/heatmap/HeatmapRenderer.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RadialGradientPaint;
 import java.awt.geom.Point2D;
+
+import org.apache.xpath.operations.Bool;
 import org.gephi.preview.api.*;
 import org.gephi.preview.plugin.builders.NodeBuilder;
 import org.gephi.preview.plugin.items.NodeItem;
@@ -19,11 +21,12 @@ import org.openide.util.lookup.ServiceProvider;
 public class HeatmapRenderer implements Renderer {
 
     //Custom properties
-    public static final String ENABLE_NODE_GLOW = "node.glow.enable";
+    public static final String ENABLE_NODE_HEATMAP = "node.heatmap.enable";
+    //public float volume = 0;
 
     @Override
     public String getDisplayName() {
-        return NbBundle.getMessage(HeatmapRenderer.class, "GlowRenderer.name");
+        return NbBundle.getMessage(HeatmapRenderer.class, "HeatmapRenderer.name");
     }
 
     @Override
@@ -48,25 +51,47 @@ public class HeatmapRenderer implements Renderer {
 
     public void renderJava2D(Item item, G2DTarget target, PreviewProperties properties) {
 
+        PreviewProperty[] props = properties.getProperties();
+
+
         //Params
         Float x = item.getData(NodeItem.X);
         Float y = item.getData(NodeItem.Y);
-        Float size = item.getData(NodeItem.SIZE);
-        Color color = item.getData(NodeItem.COLOR);
-        Color startColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), 255);
-        Color endColor = new Color(startColor.getRed(), startColor.getGreen(), startColor.getBlue(), 0);
-        float radius = size * 6;
+        //Float size = item.getData(NodeItem.SIZE);
+        Color color;
 
+        //to get a prop, a later a value(color) as it didn't other way
+        PreviewProperty colorProp = null;
+        boolean colorsEnabled = false;
+        for(PreviewProperty prop : props){
+            if(prop.getName().equals("node.color.enable")){
+                colorsEnabled = prop.getValue();
+            }
+            if(prop.getName().equals("node.color")){
+                colorProp = prop;
+            }
+        }
+
+        if(!colorsEnabled) {
+            color = item.getData(NodeItem.COLOR);
+        }
+        else {
+            color = colorProp.getValue();
+        }
+        Color startColor = new Color(color.getRed(), color.getGreen(), color.getBlue(),  255);
+        Color endColor = new Color(startColor.getRed(), startColor.getGreen(), startColor.getBlue(), 0);
+       //volume = size * 6;
+        float volume = props[1].getValue();
         //Get Java2D canvas
         Graphics2D g2 = target.getGraphics();
-        RadialGradientPaint p = new RadialGradientPaint(new Point2D.Double(x, y), radius,
+        RadialGradientPaint p = new RadialGradientPaint(new Point2D.Double(x, y), volume,
                 new float[]{
                     0.0f, 1.0f},
                 new Color[]{
                     startColor,
                     endColor});
         g2.setPaint(p);
-        g2.fillOval((int) (x - radius), (int) (y - radius), (int) (radius * 2), (int) (radius * 2));
+        g2.fillOval((int) (x - volume), (int) (y - volume), (int) (volume * 2), (int) (volume * 2));
     }
 
     public void renderPDF(Item item, PDFTarget target, PreviewProperties properties) {
@@ -79,21 +104,41 @@ public class HeatmapRenderer implements Renderer {
 
     @Override
     public PreviewProperty[] getProperties() {
-        return new PreviewProperty[]{
-            PreviewProperty.createProperty(this, ENABLE_NODE_GLOW, Boolean.class,
-            "Show heatmap effect",
-            "Heatmap effect",
-            PreviewProperty.CATEGORY_NODES).setValue(false)
+
+
+
+        return new PreviewProperty[]{ //heatmapProp, volumeProp, enableColorProp, colorProp
+                PreviewProperty.createProperty(this, ENABLE_NODE_HEATMAP, Boolean.class,
+                "Show heatmap effect",
+                "Heatmap effect",
+                "Heatmap").setValue(false),
+
+            PreviewProperty.createProperty(this, "node.volume", Float.class,
+                "Volume",
+                "Sets the volume of node for heatmap effect",
+                "Heatmap").setValue(100.0f),
+
+            PreviewProperty.createProperty(this, "node.color.enable", Boolean.class,
+                "Enable nodes color",
+                "Enables one color for all nodes when generating heatmap",
+                "Heatmap",
+                ENABLE_NODE_HEATMAP).setValue(false),
+
+            PreviewProperty.createProperty(this, "node.color", Color.class,
+                "Color",
+                "Sets color of nodes",
+                "Heatmap",
+                "node.color.enable").setValue(Color.RED)
         };
     }
 
     @Override
     public boolean isRendererForitem(Item item, PreviewProperties properties) {
-        return item instanceof NodeItem && properties.getBooleanValue(ENABLE_NODE_GLOW);
+        return item instanceof NodeItem && properties.getBooleanValue(ENABLE_NODE_HEATMAP);
     }
 
     @Override
     public boolean needsItemBuilder(ItemBuilder itemBuilder, PreviewProperties properties) {
-        return itemBuilder instanceof NodeBuilder && properties.getBooleanValue(ENABLE_NODE_GLOW);
+        return itemBuilder instanceof NodeBuilder && properties.getBooleanValue(ENABLE_NODE_HEATMAP);
     }
 }
